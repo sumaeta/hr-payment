52dcdc330eb7a54349e1bbb142a468d14a29ddbf
sumaeta <brenosumaeta33@gmail.com>
2022-09-23 19:36:52 -0300

    First Commit

---
+++ hr-payroll/src/main/java/com/dev/hrpayroll/cloud/WorkerFeignClient.java
@@ -0,0 +1,17 @@
+package com.dev.hrpayroll.cloud;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.dev.hrpayroll.modal.Worker;
+
+@Component
+@FeignClient(name = "hr-worker", path="/workers")
+public interface WorkerFeignClient {
+
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findById(@PathVariable long id);
+}
