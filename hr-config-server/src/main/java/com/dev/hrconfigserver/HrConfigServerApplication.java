f43bb67f4f5520e8722b71a71cfc81d9e4a33399
sumaeta <brenosumaeta33@gmail.com>
2022-09-26 17:37:13 -0300

    Config em repositório privativo

--- hr-config-server/src/main/java/com/dev/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/dev/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,27 @@
 package com.dev.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
 
+	@Value("${spring.cloud.config.server.git.username}")
+	private String user;
+	@Value("${spring.cloud.config.server.git.password}")
+	private String pass;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("USER: " + user + " PASS: " + pass
+		);
+	}
 }
